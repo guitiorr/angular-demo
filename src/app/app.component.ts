23bc7e466ea4b116cb3661bc3737b19918c0d18d
guitiorr <officialglude12345@gmail.com>
2025-02-13 11:06:44 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from "./rooms/rooms.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoomsComponent],
   templateUrl: './app.component.html',
+  // template: `<h1>Hello world from inline template<h1>
+  // <p>I like turles</p>
+  // `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
