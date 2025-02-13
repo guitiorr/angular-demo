abf162966631e7a3c2cfd6ada0ebdcc4679cbbae
guitiorr <officialglude12345@gmail.com>
2025-02-13 14:42:23 +0700

    if, for, switch diretives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
+import {CommonModule} from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, RoomsComponent],
+  imports: [RouterOutlet, RoomsComponent, CommonModule],
   templateUrl: './app.component.html',
   // template: `<h1>Hello world from inline template<h1>
   // <p>I like turles</p>
@@ -13,4 +15,6 @@ import { RoomsComponent } from "./rooms/rooms.component";
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  role: String = 'Admin';
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -15,15 +17,19 @@ export class RoomsComponent {
 
   hideRooms = false;
 
-  rooms : Room = {
+  roomList: RoomList[] = [
+    {roomNumber: 101, roomType: 'Deluxe', amenities: 'Air Conditioning, Free Wi-Fi', price: 100, photos: 'https://via.placeholder.com/150', checkinTime: new Date('2025-02-13'), checkoutTime: new Date('2025-02-14')},
+    {roomNumber: 102, roomType: 'Suite', amenities: 'Air Conditioning, Free Wi-Fi, Kitchen', price: 150, photos: 'https://via.placeholder.com/150', checkinTime: new Date('2025-02-13'), checkoutTime: new Date('2025-02-14')},
+    {roomNumber: 103, roomType: 'Standard', amenities: 'Free Wi-Fi', price: 80, photos: 'https://via.placeholder.com/150', checkinTime: new Date('2025-02-13'), checkoutTime: new Date('2025-02-14')}
+  ];
+
+  rooms: Room = {
     availableRooms: 10,
     bookedRooms: 5,
     totalRooms: 20
-  }
+  };
 
-
-
-  toggle(){
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
 
