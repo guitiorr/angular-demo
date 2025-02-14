112ada882628ed6ca5bf6fd0910b6bd940181018
guitiorr <officialglude12345@gmail.com>
2025-02-14 15:20:15 +0700

    afterviewinit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
 import {CommonModule, NgFor, NgIf} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RoomsComponent, CommonModule, NgIf, NgFor],
+  imports: [RoomsComponent, HeaderComponent, CommonModule, NgIf, NgFor],
   templateUrl: './app.component.html',
   // template: `<h1>Hello world from inline template<h1>
   // <p>I like turles</p>
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, viewChild } from '@angular/core';
 import { CommonModule, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { Room, RoomList } from './rooms';
 import { RoomsListComponent } from './rooms-list/rooms-list.component';
 import { DatePipe, LowerCasePipe, CurrencyPipe, DecimalPipe } from '@angular/common';
-
+import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-rooms',
-  imports: [RoomsListComponent, DatePipe, LowerCasePipe, CurrencyPipe, DecimalPipe, NgFor, NgIf, JsonPipe],
+  imports: [RoomsListComponent, HeaderComponent, DatePipe, LowerCasePipe, CurrencyPipe, DecimalPipe, NgFor, NgIf, JsonPipe],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
-export class RoomsComponent {
+export class RoomsComponent implements AfterViewInit, OnInit{
+
+  @ViewChild(HeaderComponent, {static: true}) headerComponent!: HeaderComponent;
 
   hotelName = 'Hilton Hotel';
 
@@ -43,11 +45,17 @@ export class RoomsComponent {
   }
 
   ngOnInit(): void{
+    console.log(this.headerComponent)
+
     this.roomList = [
       {roomNumber: 101, roomType: 'Deluxe', amenities: 'Air Conditioning, Free Wi-Fi', price: 100, photos: 'https://via.placeholder.com/150', checkinTime: new Date('2025-02-13'), checkoutTime: new Date('2025-02-14'), rating: 4.5},
       {roomNumber: 102, roomType: 'Suite', amenities: 'Air Conditioning, Free Wi-Fi, Kitchen', price: 150, photos: 'https://via.placeholder.com/150', checkinTime: new Date('2025-02-13'), checkoutTime: new Date('2025-02-14'), rating: 3.4},
       {roomNumber: 103, roomType: 'Standard', amenities: 'Free Wi-Fi', price: 80, photos: 'https://via.placeholder.com/150', checkinTime: new Date('2025-02-13'), checkoutTime: new Date('2025-02-14'), rating: 5}
     ]
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.headerComponent);
   }
 
   selectRoom(room: RoomList){
