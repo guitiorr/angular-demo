c615352f3d714bb8e9dc20f783d16b619bb85baa
guitiorr <officialglude12345@gmail.com>
2025-02-17 08:42:58 +0700

    add container and employee component, use ng-content in container component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
 import {CommonModule, NgFor, NgIf} from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RoomsComponent, HeaderComponent, CommonModule, NgIf, NgFor],
+  imports: [RoomsComponent, HeaderComponent, ContainerComponent, CommonModule, NgIf, NgFor],
   templateUrl: './app.component.html',
   // template: `<h1>Hello world from inline template<h1>
   // <p>I like turles</p>
